e1016d428811f71902a81dc7e9df35f81db7055b
Joe Chin <joe.chin.jia.hao@gmail.com>
2022-01-18 09:36:33 +0800

    first

---
+++ src/test/java/com/example/bookSearch/BookSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
